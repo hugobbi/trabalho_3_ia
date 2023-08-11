d04605e92b65a0d73dcb1eadfc00207707d1457d
hugobbi <hgugobbi@gmail.com>
2023-08-11 13:22:13 -0300

    Q3

--- solucao.py
+++ solucao.py
@@ -4,7 +4,7 @@ class Nodo:
     """
     Implemente a classe Nodo com os atributos descritos na funcao init
     """
-    def __init__(self, estado:str, pai, acao:str | None, custo:int):
+    def __init__(self, estado:str, pai:"Nodo" or None, acao:str or None, custo:int):
         """
         Inicializa o nodo com os atributos recebidos
         :param estado:str, representacao do estado do 8-puzzle
@@ -17,7 +17,7 @@ class Nodo:
         self.pai = pai
         self.custo = custo
     
-    def __eq__(self, outro) -> bool:
+    def __eq__(self, outro: "Nodo") -> bool:
         if isinstance(self, outro):
             return (self.estado == outro.estado and self.acao == outro.acao and self.pai == outro.pai and self.custo == outro.custo) # pode dar ruim a comparacao do pai
         return False
@@ -25,21 +25,23 @@ class Nodo:
     def __hash__(self) -> int:
         return hash((self.estado, self.acao, self.pai, self.custo)) # pode dar ruim o hash do pai
     
-    def get_estado(self): 
+    def get_estado(self) -> str: 
         return self.estado
     
-    def get_acao(self): 
+    def get_acao(self) -> str: 
         return self.acao
     
-    def get_pai(self): 
+    def get_pai(self) -> "Nodo": 
         return self.pai
     
-    def get_custo(self): 
+    def get_custo(self) -> int: 
         return self.custo
     
-    def incrementa_custo(self):
-        self.custo += 1
-        return
+    def calcula_custo(self) -> int: # custo de uma ação para um nodo filho futuro
+        return self.get_custo + 1
+    
+    def isRoot(self) -> bool:
+        return self.pai is None
 
 def swap(string: str, idx1: int, idx2: int) -> str:
     string_lista = list(string)
@@ -85,18 +87,23 @@ def sucessor(estado:str) -> Set[Tuple[str,str]]:
     return possiveis_estados
 
 
-def expande(nodo:Nodo)->Set[Nodo]:
+def expande(nodo:Nodo) -> Set[Nodo]:
     """
     Recebe um nodo (objeto da classe Nodo) e retorna um conjunto de nodos.
     Cada nodo do conjunto é contém um estado sucessor do nó recebido.
     :param nodo: objeto da classe Nodo
     :return:
     """
-    # substituir a linha abaixo pelo seu codigo
-    raise NotImplementedError
+    estado_nodo = nodo.get_estado()
+    possiveis_estados = sucessor(estado_nodo)
+    nodos_filhos = set()
+    for estado in possiveis_estados:
+        nodo_filho = Nodo(estado[1], nodo, estado[0], nodo.calcula_custo())
+        nodos_filhos.add(nodo_filho)
+    return nodos_filhos
 
 
-def astar_hamming(estado:str)->list[str]:
+def astar_hamming(estado:str) -> list[str]:
     """
     Recebe um estado (string), executa a busca A* com h(n) = soma das distâncias de Hamming e
     retorna uma lista de ações que leva do
@@ -109,7 +116,7 @@ def astar_hamming(estado:str)->list[str]:
     raise NotImplementedError
 
 
-def astar_manhattan(estado:str)->list[str]:
+def astar_manhattan(estado:str) -> list[str]:
     """
     Recebe um estado (string), executa a busca A* com h(n) = soma das distâncias de Manhattan e
     retorna uma lista de ações que leva do
@@ -121,7 +128,7 @@ def astar_manhattan(estado:str)->list[str]:
     # substituir a linha abaixo pelo seu codigo
     raise NotImplementedError
 
-def bfs(estado:str)->list[str]:
+def bfs(estado:str) -> list[str]:
     """
     Recebe um estado (string), executa a busca em LARGURA e
     retorna uma lista de ações que leva do
@@ -134,7 +141,7 @@ def bfs(estado:str)->list[str]:
     raise NotImplementedError
 
 
-def dfs(estado:str)->list[str]:
+def dfs(estado:str) -> list[str]:
     """
     Recebe um estado (string), executa a busca em PROFUNDIDADE e
     retorna uma lista de ações que leva do
@@ -146,7 +153,7 @@ def dfs(estado:str)->list[str]:
     # substituir a linha abaixo pelo seu codigo
     raise NotImplementedError
 
-def astar_new_heuristic(estado:str)->list[str]:
+def astar_new_heuristic(estado:str) -> list[str]:
     """
     Recebe um estado (string), executa a busca A* com h(n) = sua nova heurística e
     retorna uma lista de ações que leva do
